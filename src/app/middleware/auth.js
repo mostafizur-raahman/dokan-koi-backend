f9904896fa1b1f8d31cfb35fe4316e769005f586
mostafizur-raahman <mostafizurrahman@0202@gmail.com>
2024-06-04 23:12:07 +0600

    change auth and add req.user._id

--- src/app/middleware/auth.js
+++ src/app/middleware/auth.js
@@ -1,14 +1,28 @@
 // middleware/auth.js
 import jwt from "jsonwebtoken";
 import config from "../config/index.js";
-const auth = (req, res, next) => {
+import { UserServices } from "../../modules/user/user.service.js";
+
+const auth = async (req, res, next) => {
     const token = req.header("Authorization");
 
     if (!token) return res.status(401).json({ message: "Login required" });
 
     try {
         const decoded = jwt.verify(token, config.jwt_secret_key);
-        req.user = decoded;
+
+        const user = await UserServices.findById(decoded._id);
+
+        if (!user) {
+            return res
+                .status(401)
+                .json({ success: false, message: "User not found" });
+        }
+        console.log("user ", user);
+        console.log("decoded", decoded);
+
+        req.user = user;
+
         next();
     } catch (error) {
         res.status(400).json({ error: "Invalid token" });
